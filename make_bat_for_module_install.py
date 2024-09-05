04dd14b4398e208f42068f798676b4892509e4a6
bluenote79 <joerg@niederhoff.com>
2024-09-05 15:48:12 +0200

    Add files via upload
    Since external modules numpy and pyfoil are used with the fusion python, this script started in fusion 360 gets the path of the python interpreter an creates two bat-files on windows desktop to install numpy and pyfoil into fusion.

---
+++ make_bat_for_module_install.py
@@ -0,0 +1,51 @@
+#Author-
+#Description-
+
+import adsk.core, adsk.fusion, adsk.cam, traceback
+
+import sys
+import os
+
+
+app = adsk.core.Application.get()
+ui = None
+ui  = app.userInterface
+
+import sys
+
+
+# abslolute_path = os.path.abspath("~/lib").as_posix()
+
+def run(context):
+
+    # get path of skript
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    
+    py_ex = os.path.abspath(str(sys.executable))
+
+    numpy_install = f'{py_ex}.exe -m pip install --upgrade numpy'
+
+    pyfoil_install = f'{py_ex}.exe -m pip install --upgrade pyfoil'
+
+    text1 = "start cmd /K " + numpy_install
+    text2 = "start cmd /K " + pyfoil_install
+
+
+    desktop = os.path.normpath(os.path.expanduser("~/Desktop"))
+
+    
+    filename1 = os.path.join(desktop, "numpy.bat")
+
+    file1 = open(filename1, 'w')
+    file1.close()
+    filea1 = open(filename1, "a")
+    filea1.write(text1)
+
+
+    filename2 = os.path.join(desktop, "pyfoil.bat")
+
+    file2 = open(filename2, 'w')
+    file2.close()
+    filea2 = open(filename2, "a")
+    filea2.write(text2)
+
